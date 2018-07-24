3f56154c6b2b57d5743873e8a1114b35b31cf0c6
Coursera Learner <nobody@nowhere.nul>
2018-07-24 14:07:54 +0000

    added squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// definig a max func
+
+// this is a shortened if statment to compare between value
+
+
+
+int  max(int a, int b ){  if((a) > (b)) return  (a); else return  (b);}  
+
+int min(int a,int b ){if((a) < (b)) return  (a); else return (b) ;}
+
+
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w= x_offset + size2;
+  int w =max(size1, ( x_offset + size2)) ;
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h = y_offset + size2;
+  int h = max(size1,( y_offset + size2)) ;
   //count from 0 to h. Call the number you count with y
-  for ( int y = 0;y < h;y++){
+  for ( int y = 0; y < h ; y++){
     //count from 0 to w. Call the number you count with x
-    for (int x = 0 ; x< w;x++){
+    for (int x = 0 ; x < w ; x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -18,13 +29,20 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if (((x_offset <= x && w > x)&&( y == y_offset || y == h-1   )) || ((y_offset <= y && h > y)&&( x == x_offset || x == w-1 ))  )
-	printf("*");
-      else if((y < size1 && ((x== 0 )|| (x == size1-1))) || (x < size1 && ((y== 0 )|| (y == size1-1)))  )
-        printf("#");
+      if (((x_offset <= x && x_offset +size2 > x)&&( y == y_offset || y == y_offset + size2 - 1   )) ||
+	  ((y_offset < y && y_offset + size2 > y)&&( x == x_offset || x == x_offset + size2 -1 )) )
+	  printf("*");
+
+
+      else if(       (  x < size1 && ( y == 0 || y == size1-1)  )     ||  (   y < size1 &&  ( x== 0 ||  x == size1-1 )) )
+	  printf("#");
+	
+	 
       else
 	printf(" ");
+     
     }
+    
     printf("\n");
   }
       //if not,
